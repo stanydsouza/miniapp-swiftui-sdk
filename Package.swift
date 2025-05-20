8abeb67afd7e178fc3882a1103b4a077f7d2591f
Stany Dsouza <stanydsouza93@gmail.com>
2025-05-20 12:44:48 +0530

    Initial Release: 0.0.1

--- Package.swift
+++ Package.swift
@@ -5,17 +5,35 @@ import PackageDescription
 
 let package = Package(
     name: "miniapp-swiftui-sdk",
+    platforms: [
+        .iOS(.v16),
+        .tvOS(.v16),
+        .watchOS(.v9),
+        .macOS(.v13),
+        .macCatalyst(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "miniapp-swiftui-sdk",
-            targets: ["miniapp-swiftui-sdk"]),
+            targets: ["miniapp-swiftui-sdk", "miniapp_swiftui"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/stanydsouza/miniapp-core-sdk.git", .upToNextMajor(from: "0.0.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "miniapp-swiftui-sdk"),
-
+            name: "miniapp-swiftui-sdk",
+            dependencies: [
+                .product(name: "miniapp-core-sdk", package: "miniapp-core-sdk")
+            ]
+        ),
+        .binaryTarget(
+            name: "miniapp_swiftui",
+            path: "./Sources/miniapp_swiftui.xcframework"
+        )
     ]
 )
